f979b7d24634359a2685417392a27a214c882abe
AbdulhakimUS <127390486+AbdulhakimAmonov@users.noreply.github.com>
2023-11-06 21:02:37 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var buttons = document.querySelectorAll(".drum")
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(){
+        alert("Qontaysan Bratish")
+    })
+        
+}
+
